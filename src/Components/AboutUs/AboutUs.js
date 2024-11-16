1100f5a0393d67d4ead2933b1f2b430eb387d49e
AKAspidey01 <sg654415@gmail.com>
2024-11-16 13:29:43 +0530

    new-push

---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './AboutUs.scss';
+
+const AboutUs = () => (
+  <div className="AboutUs">
+    AboutUs Component
+  </div>
+);
+
+AboutUs.propTypes = {};
+
+AboutUs.defaultProps = {};
+
+export default AboutUs;
